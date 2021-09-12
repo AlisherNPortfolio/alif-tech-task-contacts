22f5fee68d5f289771d9d817329d6e89d3ae356c
Nasrullayev Alisher <myemail9445@inbox.ru>
2021-09-12 12:57:37 +0500

    contacts controller and route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('test', function () {
     return 'data';
 });
+
+Route::resource('user', UserController::class);

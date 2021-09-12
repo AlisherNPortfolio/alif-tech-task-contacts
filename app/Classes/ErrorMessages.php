2806f1f661ae664e0e8293be3658860def38f689
Nasrullayev Alisher <myemail9445@inbox.ru>
2021-09-12 18:16:01 +0500

    error messages class

---
+++ app/Classes/ErrorMessages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Classes;
+
+class ErrorMessages
+{
+    public const NOT_FOUND = 404;
+    public const NOT_FOUND_MSG = 'Resource not found!';
+
+    public const CAN_NOT_DELETE = 1000;
+    public const CAN_NOT_DELETE_MSG = 'Resource could not be deleted!';
+
+    public const CAN_NOT_CREATE = 1001;
+    public const CAN_NOT_CREATE_MSG = 'Resource could not be created!';
+}
